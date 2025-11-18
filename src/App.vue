8af6a92291328e9b6bafc9ff010294f9532be8b7
Alex S <kazzon95@gmail.com>
2025-11-18 11:49:50 +0300

    Домашнее задание 3 Шаблоны

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const listShow = ref(true)
+const showAge = ref(true)
+
+const hover = ref({
+	color: 'fuchsia'
+})
+
+const persons = ref(
+	[
+		{
+			name: 'Саша',
+			year: 30,
+		},
+		{
+			name: 'Лиза',
+			year: 29,
+		},
+		{
+			name: 'Ваня',
+			year: 28,
+		},
+		{
+			name: 'Надя',
+			year: 22,
+		},
+		{
+			name: 'Вова',
+			year: 16
+		},
+	]
+)
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<button class="close-list" @click="listShow = !listShow">{{ listShow ? 'Скрыть список' : 'Показать список' }}</button>
+	<button class="toggle-age" @click="showAge = !showAge">
+			{{ showAge ? 'Скрыть возраст' : 'Показать возраст' }}
+	</button>
+	<ul v-if="listShow">
+		<li 
+			v-for="(person, index) in persons" 
+			:key="index"
+			class="element"
+		>
+			{{ person.name }}
+			 <span v-if="showAge"> - {{ person.year }} лет</span>
+		</li>
+	</ul>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+ul {
+	list-style-type: none;
+	padding-left: 0px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+li, .close-list {
+	cursor: pointer;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.element:hover {
+	color: v-bind('hover.color');
 }
 </style>
